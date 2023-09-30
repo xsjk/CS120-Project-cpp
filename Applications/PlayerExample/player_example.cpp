7f5af2f2fab8d0f65eab3ad98224220b9b2babc4
xsjk <xsjk17@outlook.com>
2023-10-01 01:57:14 +0800

    remove JUCE dependency

--- Applications/PlayerExample/player_example.cpp
+++ Applications/PlayerExample/player_example.cpp
@@ -1,77 +1,108 @@
-/*
-  ==============================================================================
+// Audio Stream Input/Output library
+#include <iostream>
+#include <cmath>
+#include "asiosys.h"
+#include "asio.h"
+#include "asiodrivers.h"
+#include "Windows.h"
 
-    This file contains the basic startup code for a JUCE application.
-
-  ==============================================================================
-*/
-
-#include <JuceHeader.h>
-
-#define PI acos(-1)
-#define SAMPLE_RATE 44100
-
-using namespace juce;
-
-class Tester : public AudioIODeviceCallback {
-
-    float phase = 0.0f;
-    int freq = 440; // Hz
-    float amp = 0.7f;
-    float sampleRate = SAMPLE_RATE;
-    int channelNum = 1;
-    float dPhasePerSample = 2 * PI * (freq / sampleRate);
-
-public:
-    Tester() {}
-
-    void audioDeviceAboutToStart(AudioIODevice* device) override {}
-
-    void audioDeviceStopped() override {}
+#define M_PI 3.14159265358979323846
 
 
-    void audioDeviceIOCallbackWithContext(
-        const float* const* inputChannelData,
-        int numInputChannels,
-        float* const* outputChannelData,
-        int numOutputChannels,
-        int numSamples,
-        const AudioIODeviceCallbackContext& context
-    ) override {
-        for (int channel = 0; channel < numOutputChannels; ++channel) {
-            for (int i = 0; i < numSamples; i++) {
-                outputChannelData[channel][i] = amp * sin(phase += dPhasePerSample);
-            }
-        }
-    }
+#define CATCH_ERROR(API) \
+    do { \
+        ASIOError result = API; \
+        if (result != ASE_OK) { \
+            std::cerr << #API << " failed with error code " << result << std::endl; \
+            throw std::runtime_error(#API " failed"); \
+        } \
+    } while (0)
 
 
+ASIOBufferInfo input_buffer_info = { .isInput = ASIOTrue };
+ASIOBufferInfo output_buffer_info = { .isInput = ASIOFalse };
+ASIOChannelInfo input_channel_info;
+ASIOChannelInfo output_channel_info;
+long buffer_size;
+
+// ASIO driver buffer switch function
+void buffer_switch(long index, ASIOBool process_now) {
+
+}
+
+
+ASIOCallbacks asio_callbacks = {
+    .bufferSwitch = buffer_switch,
 };
 
-//==============================================================================
-int main(int argc, char* argv[])
-{
-    /* Initialize Player */
-    AudioDeviceManager dev_manager;
-    dev_manager.initialiseWithDefaultDevices(1,1);
-    AudioDeviceManager::AudioDeviceSetup dev_info;
-    dev_info = dev_manager.getAudioDeviceSetup();
-    dev_info.sampleRate = SAMPLE_RATE;
-    dev_manager.setAudioDeviceSetup(dev_info, false);
 
-    /* Add callback to AudioDeviceManager */
-    std::unique_ptr<Tester> tester;
-    if (tester.get() == nullptr)
-    {
-        tester.reset(new Tester());
-        dev_manager.addAudioCallback(tester.get());
-    }
 
-    /* Terminate the process */
-    std::cout << "Press any ENTER to stop.\n";
-    getchar();
-    dev_manager.removeAudioCallback(tester.get());
+void display_info() {
+
+    // display information
+    long num_input_channels, num_output_channels;
+    CATCH_ERROR(ASIOGetChannels(&num_input_channels, &num_output_channels));
+    std::cout << "Number of input channels: " << num_input_channels << std::endl;
+    std::cout << "Number of output channels: " << num_output_channels << std::endl;
+
+    long input_latency, output_latency;
+    CATCH_ERROR(ASIOGetLatencies(&input_latency, &output_latency));
+    std::cout << "Input latency: " << input_latency << " samples" << std::endl;
+    std::cout << "Output latency: " << output_latency << " samples" << std::endl;
+
+    long min_buffer_size, max_buffer_size, preferred_buffer_size, buffer_size_granularity;
+    CATCH_ERROR(ASIOGetBufferSize(&min_buffer_size, &max_buffer_size, &preferred_buffer_size, &buffer_size_granularity));
+    std::cout << "Minimum buffer size: " << min_buffer_size << " samples" << std::endl;
+    std::cout << "Maximum buffer size: " << max_buffer_size << " samples" << std::endl;
+    std::cout << "Preferred buffer size: " << preferred_buffer_size << " samples" << std::endl;
+    std::cout << "Buffer size granularity: " << buffer_size_granularity << " samples" << std::endl;
+
+    ASIOSampleRate sample_rate = 48000;
+    CATCH_ERROR(ASIOCanSampleRate(sample_rate));
+    CATCH_ERROR(ASIOGetSampleRate(&sample_rate));
+    std::cout << "Current sample rate: " << sample_rate << " Hz" << std::endl;
+    // CATCH_ERROR(ASIOSetSampleRate(sample_rate));
+
+    ASIOClockSource clock_source;
+    long num_clock_sources;
+    CATCH_ERROR(ASIOGetClockSources(&clock_source, &num_clock_sources));
+    std::cout << "Number of clock sources: " << num_clock_sources << std::endl;
+    std::cout << "Clock source: " << clock_source.name << std::endl;
+
+    ASIOSamples sample_position;
+    ASIOTimeStamp sample_time_stamp;
+    CATCH_ERROR(ASIOGetSamplePosition(&sample_position, &sample_time_stamp));
+    std::cout << "Sample position: " << sample_position.hi << " " << sample_position.lo << std::endl;
+    std::cout << "Sample time stamp: " << sample_time_stamp.hi << " " << sample_time_stamp.lo << std::endl;
+
+
+    // CATCH_ERROR(ASIOControlPanel());
+    // std::cout << "Control panel opened" << std::endl;
+
+}
+// Main function
+int main() {
+
+    AsioDrivers drivers;
+    drivers.loadDriver("ASIO4ALL v2");
+
+    // Initialize ASIO driver
+    ASIODriverInfo asio_driver_info;
+    CATCH_ERROR(ASIOInit(&asio_driver_info));
+
+    CATCH_ERROR(ASIOCreateBuffers(&input_buffer_info, 1, 1024, &asio_callbacks));
+    CATCH_ERROR(ASIOCreateBuffers(&output_buffer_info, 1, 1024, &asio_callbacks));
+    
+    display_info();
+
+    // Start ASIO driver
+    CATCH_ERROR(ASIOStart());
+    
+    // Wait 1s
+    Sleep(1000);
+    
+    CATCH_ERROR(ASIODisposeBuffers());
+    CATCH_ERROR(ASIOStop());
 
     return 0;
 }
-
--- Libraries/ASIO-SDK/common/register.cpp
+++ Libraries/ASIO-SDK/common/register.cpp
@@ -29,8 +29,8 @@ LONG RegisterAsioDriver (CLSID,char *,char *,char *,char *);
 LONG UnregisterAsioDriver (CLSID,char *,char *);
 
 static LONG findRegPath (HKEY,char *);
-static LONG createRegPath (HKEY,char *,char *);
-static LONG createRegStringValue (HKEY,char *,char *,char *);
+static LONG createRegPath (HKEY,char *,const char *);
+static LONG createRegStringValue (HKEY,char *,const char *,char *);
 static LONG getRegString (HKEY,char *,char *,LPVOID,DWORD);
 static LPKEYLIST findAllSubKeys (HKEY,HKEY,DWORD,char *,LPKEYLIST);
 static LONG deleteRegPath (HKEY,char *,char *);
@@ -195,7 +195,7 @@ static LONG findRegPath (HKEY mainkey,char *szregpath)
 	return rc;
 }
 
-static LONG createRegPath (HKEY mainkey,char *szregpath,char *sznewpath)
+static LONG createRegPath (HKEY mainkey,char *szregpath,const char *sznewpath)
 {
 	HKEY	hkey,hksub;
 	LONG 	cr,rc = -1;
@@ -216,7 +216,7 @@ static LONG createRegPath (HKEY mainkey,char *szregpath,char *sznewpath)
 	return rc;
 }
 
-static LONG createRegStringValue (HKEY mainkey,char *szregpath,char *valname,char *szvalstr)
+static LONG createRegStringValue (HKEY mainkey,char *szregpath,const char *valname,char *szvalstr)
 {
 	LONG	cr,rc = -1;
 	HKEY	hkey;
