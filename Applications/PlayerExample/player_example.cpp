98f3d167408a70dec62ab5348e5e2c7884210e92
xsjk <xsjk17@outlook.com>
2023-10-01 16:44:07 +0800

    simplify the process

--- Applications/PlayerExample/player_example.cpp
+++ Applications/PlayerExample/player_example.cpp
@@ -1,108 +1,69 @@
 // Audio Stream Input/Output library
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <numbers>
 #include "asiosys.h"
+#include "asiodrvr.h"
 #include "asio.h"
-#include "asiodrivers.h"
 #include "Windows.h"
-
-#define M_PI 3.14159265358979323846
-
+#include "asiodrivers.h"
 
 #define CATCH_ERROR(API) \
     do { \
         ASIOError result = API; \
         if (result != ASE_OK) { \
             std::cerr << #API << " failed with error code " << result << std::endl; \
-            throw std::runtime_error(#API " failed"); \
+            throw std::runtime_error(#API " failed at " __FILE__ ":" + std::to_string(__LINE__)); \
         } \
     } while (0)
 
 
-ASIOBufferInfo input_buffer_info = { .isInput = ASIOTrue };
-ASIOBufferInfo output_buffer_info = { .isInput = ASIOFalse };
-ASIOChannelInfo input_channel_info;
-ASIOChannelInfo output_channel_info;
-long buffer_size;
+ASIOBufferInfo bufInfo[4];
 
-// ASIO driver buffer switch function
-void buffer_switch(long index, ASIOBool process_now) {
-
+static void convertFromFloat (const float* src, int* dest, int n) noexcept {
+    constexpr double maxVal = 0x7fffffff;
+    while (--n >= 0) {
+        auto val = maxVal * *src++;
+        *dest++ = round(val < -maxVal ? -maxVal : (val > maxVal ? maxVal : val));
+    }
+}
+static void convertToFloat (const int* src, float* dest, int n) noexcept {
+    constexpr double g = 1. / 0x7fffffff;
+    while (--n >= 0) *dest++ = g * *src++;
 }
 
-
-ASIOCallbacks asio_callbacks = {
-    .bufferSwitch = buffer_switch,
-};
-
-
-
-void display_info() {
-
-    // display information
-    long num_input_channels, num_output_channels;
-    CATCH_ERROR(ASIOGetChannels(&num_input_channels, &num_output_channels));
-    std::cout << "Number of input channels: " << num_input_channels << std::endl;
-    std::cout << "Number of output channels: " << num_output_channels << std::endl;
-
-    long input_latency, output_latency;
-    CATCH_ERROR(ASIOGetLatencies(&input_latency, &output_latency));
-    std::cout << "Input latency: " << input_latency << " samples" << std::endl;
-    std::cout << "Output latency: " << output_latency << " samples" << std::endl;
-
-    long min_buffer_size, max_buffer_size, preferred_buffer_size, buffer_size_granularity;
-    CATCH_ERROR(ASIOGetBufferSize(&min_buffer_size, &max_buffer_size, &preferred_buffer_size, &buffer_size_granularity));
-    std::cout << "Minimum buffer size: " << min_buffer_size << " samples" << std::endl;
-    std::cout << "Maximum buffer size: " << max_buffer_size << " samples" << std::endl;
-    std::cout << "Preferred buffer size: " << preferred_buffer_size << " samples" << std::endl;
-    std::cout << "Buffer size granularity: " << buffer_size_granularity << " samples" << std::endl;
-
-    ASIOSampleRate sample_rate = 48000;
-    CATCH_ERROR(ASIOCanSampleRate(sample_rate));
-    CATCH_ERROR(ASIOGetSampleRate(&sample_rate));
-    std::cout << "Current sample rate: " << sample_rate << " Hz" << std::endl;
-    // CATCH_ERROR(ASIOSetSampleRate(sample_rate));
-
-    ASIOClockSource clock_source;
-    long num_clock_sources;
-    CATCH_ERROR(ASIOGetClockSources(&clock_source, &num_clock_sources));
-    std::cout << "Number of clock sources: " << num_clock_sources << std::endl;
-    std::cout << "Clock source: " << clock_source.name << std::endl;
-
-    ASIOSamples sample_position;
-    ASIOTimeStamp sample_time_stamp;
-    CATCH_ERROR(ASIOGetSamplePosition(&sample_position, &sample_time_stamp));
-    std::cout << "Sample position: " << sample_position.hi << " " << sample_position.lo << std::endl;
-    std::cout << "Sample time stamp: " << sample_time_stamp.hi << " " << sample_time_stamp.lo << std::endl;
-
-
-    // CATCH_ERROR(ASIOControlPanel());
-    // std::cout << "Control panel opened" << std::endl;
-
+void buffer_switch(long bufferIndex, ASIOBool) {
+        
+    static float outBuffers[2][512];
+    static float inBuffers[2][512];
+    for (int i = 0; i < 2; ++i)
+        convertToFloat ((int*)bufInfo[i].buffers[bufferIndex], inBuffers[i], 512);
+    {
+        constexpr float dphase = 2 * std::numbers::pi * 440. / 44100.;
+        static float phase = 0;
+        for (int j = 0; j < 512; ++j)
+            outBuffers[0][j] = outBuffers[1][j] = sinf(phase += dphase);
+    }
+    for (int i = 0; i < 2; ++i)
+        convertFromFloat (outBuffers[i], (int*)bufInfo[2 + i].buffers[bufferIndex], 512);
 }
-// Main function
+
 int main() {
-
     AsioDrivers drivers;
+    ASIODriverInfo driver_info;
+    ASIOCallbacks callbacks = { .bufferSwitch = buffer_switch};
+
     drivers.loadDriver("ASIO4ALL v2");
+    CATCH_ERROR(ASIOInit(&driver_info));
+    for (int i = 0; i < 4; i++) {
+        bufInfo[i].channelNum = i % 2;
+        bufInfo[i].isInput = i < 2;
+    }
 
-    // Initialize ASIO driver
-    ASIODriverInfo asio_driver_info;
-    CATCH_ERROR(ASIOInit(&asio_driver_info));
-
-    CATCH_ERROR(ASIOCreateBuffers(&input_buffer_info, 1, 1024, &asio_callbacks));
-    CATCH_ERROR(ASIOCreateBuffers(&output_buffer_info, 1, 1024, &asio_callbacks));
-    
-    display_info();
-
-    // Start ASIO driver
+    CATCH_ERROR(ASIOCreateBuffers(bufInfo, 4, 512, &callbacks));
     CATCH_ERROR(ASIOStart());
-    
-    // Wait 1s
-    Sleep(1000);
-    
-    CATCH_ERROR(ASIODisposeBuffers());
-    CATCH_ERROR(ASIOStop());
 
+    Sleep(1000);
     return 0;
 }
