a7ebac7be675a5b06ccab22709fc54019e1cd6a8
xsjk <xsjk17@outlook.com>
2023-10-03 21:56:54 +0800

    record to file

--- Applications/PlayerExample/Main.cpp
+++ Applications/PlayerExample/Main.cpp
@@ -7,6 +7,8 @@
 */
 
 #include <JuceHeader.h>
+#include <numbers>
+#include <fstream>
 
 #define PI acosf(-1)
 #define SAMPLE_RATE 44100
@@ -15,15 +17,13 @@ using namespace juce;
 
 class Tester : public AudioIODeviceCallback {
 
-    int freq = 262; // Hz
-    float amp = 0.7f;
-    float sampleRate = SAMPLE_RATE;
-    int channelNum = 1;
-    float dPhasePerSample = 2 * PI * (freq / sampleRate);
-    long long step = 0;
+    std::ofstream file;
 
 public:
-    void audioDeviceAboutToStart(AudioIODevice* device) override {}
+    Tester() {
+        file.open("recorded.txt");
+    }
+    void audioDeviceAboutToStart(AudioIODevice*) override {}
 
     void audioDeviceStopped() override {}
 
@@ -37,13 +37,9 @@ public:
         const AudioIODeviceCallbackContext& context
     ) override {
         for (int i = 0; i < numSamples; i++) {
-            step++;
-            outputChannelData[0][i] = amp * (
-                + sin(2 * PI * (freq / sampleRate) * step)
-                + sin(2 * PI * (freq * 1.26 / sampleRate) * step)
-                + sin(2 * PI * (freq * 1.5 / sampleRate) * step)
-                + sin(2 * PI * (freq * 1.78 / sampleRate) * step)
-            ) / 4;
+            file << inputChannelData[0][i] << std::endl;
+            for (int channel = 0; channel < numOutputChannels; channel++)
+                outputChannelData[channel][i] = inputChannelData[0][i];
         }
     }
 
