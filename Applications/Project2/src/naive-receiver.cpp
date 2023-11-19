710f9578db6e53244b9937a3a169b696b72ebc3f
xsjk <xsjk17@outlook.com>
2023-11-19 13:05:42 +0800

    add ack for naive-receiver

--- Applications/Project2/src/naive-receiver.cpp
+++ Applications/Project2/src/naive-receiver.cpp
@@ -22,51 +22,139 @@
 #include <bitset>
 
 #include "signal.hpp"
+#include "CRC.hpp"
 
 #include "asiodevice.h"
 using namespace ASIO;
 
-std::vector<float> preamble, carrier;
-std::vector<float> rSignal;
 
 class Receiver : public IOHandler<float> {
-    void outputCallback(DataView<float> &buffer) noexcept override {
-        for (auto i = 0; i < buffer.getNumSamples(); i++) {
-            buffer(0, i) = 0;
-            buffer(1, i) = 0;
+
+    std::vector<float> preamble, carrier;
+    std::vector<float> rSignal;
+
+    const float threshold = 3;
+    const int packetBits = 500;
+    const int crcBits = 8;
+    const int carrierSize = 2;
+    const int interSize = 10;
+
+    BitsContainer rData;
+    BitsContainer rDataEncoded;
+    
+public:
+
+    Receiver() {
+
+        std::ofstream debugFile {"debug.txt"};
+        std::ifstream preambleFile {"preamble.txt"};
+        preamble.reserve(100);
+        carrier.reserve(carrierSize);
+        rSignal.reserve(200000);
+        rDataEncoded.reserve(100000);
+
+        float preambleSample;
+        while (preambleFile >> preambleSample) {
+            preamble.emplace_back(preambleSample);
+        }
+
+        for (auto i = 0; i < carrierSize; i++) {
+            carrier.emplace_back(1);
+        }
+
+    }
+
+
+    int ack_to_send = 0;
+
+    void outputCallback(DataView<float> &output) noexcept override {
+        // send ack if input succeeds
+        auto i = 0;
+        while (ack_to_send) {
+            for (; i < preamble.size(); i++)
+                output(0, i) = preamble[preamble.size() - i - 1];
+            for (; i < interSize; i++)
+                output(0, i) = 0;
+        }
+        for (; i < output.getNumSamples(); i++)
+            output(0, i) = 0;
+    }
+
+    std::deque<float> buffer;
+    CRC8<0x7> crc_checker;
+
+    int state = 0; // 0 for preamble detection and 1 for data extraction
+
+    float lastBigSumI = 0;
+
+    void inputCallback(const DataView<float> &input) noexcept override {
+        static auto sampleIndex = 0;
+        for (auto i = 0; i < input.getNumSamples(); i++, sampleIndex++) {
+            float v = input(0, i);
+
+            if (state == 0) {
+
+                buffer.emplace_back(v);
+                if (buffer.size() < preamble.size())
+                    continue;
+
+                float sum = 0;
+                for (auto j = 0; j < preamble.size(); j++)
+                    sum += buffer[j] * preamble[j];
+                buffer.pop_front();
+
+                if (sum > threshold && i - lastBigSumI > preamble.size()) {
+                    lastBigSumI = i;
+                    buffer.clear();
+                    state = 1;
+                }
+
+            } else {
+
+                buffer.emplace_back(v);
+                if (buffer.size() < (packetBits + crcBits) * carrierSize)
+                    continue;
+                for (auto sampleIndex = 0; sampleIndex < (packetBits + crcBits) * carrierSize; ) {
+                    float sum = 0;
+                    for (auto j = 0; j < (packetBits + crcBits); sampleIndex++, j++)
+                        sum += buffer[sampleIndex] * carrier[j];
+                    rDataEncoded.emplace_back(sum > 0 ? 0 : 1);
+                }
+                // check CRC
+                if (crc_checker.check(rDataEncoded.as_span<uint8_t>()) == true)
+                    ack_to_send ++;
+                buffer.clear();
+                state = 0;
+            }
         }
     }
-    void inputCallback(const DataView<float> &buffer) noexcept override {
-        static auto sampleIndex = 0;
-        for (auto i = 0; i < buffer.getNumSamples(); i++, sampleIndex++) {
-            rSignal.emplace_back(buffer(0, i));
+
+
+    ~Receiver() {
+
+        std::ofstream rSignalFile {"rSignal.txt"};
+        for (auto const &d : rSignal) {
+            rSignalFile << d << std::endl;
         }
+
+        std::cout << "Extracting ..." << std::endl;
+
+        // --------------------- apply 8b10b decoding ---------------------
+        for (auto i = 0; i < rDataEncoded.size() / 10; i++)
+            rData.push<8>(B8B10::decode(rDataEncoded.get<10>(i)));
+
+        // ----------------------------------------------------------------
+
+        std::ofstream outputDataFile("OUTPUT.bin", std::ios::binary);
+        char byteOut;
+        for (auto const &byteOut : rData.as_span<char>())
+            outputDataFile.write(&byteOut, sizeof(byteOut));
+
     }
 };
 
 
 int main() {
-
-    std::ofstream debugFile {"debug.txt"};
-    std::ifstream preambleFile {"preamble.txt"};
-    std::ofstream rSignalFile {"rSignal.txt"}, rDataFile {"rData.txt"};
-    BitsContainer rData, rDataEncoded;
-    const int packetBits = 500, carrierSize = 2;
-    preamble.reserve(100);
-    carrier.reserve(carrierSize);
-    rSignal.reserve(200000);
-    rDataEncoded.reserve(100000);
-
-    float preambleSample;
-    while (preambleFile >> preambleSample) {
-        preamble.emplace_back(preambleSample);
-    }
-
-    for (auto i = 0; i < carrierSize; i++) {
-        carrier.emplace_back(1);
-    }
-
-// ----------------------------------------------------------------
 
     auto receiver = std::make_shared<Receiver>();
     auto device = std::make_shared<Device>();
@@ -77,74 +165,9 @@ int main() {
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
     device->start(receiver);
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::this_thread::sleep_for(std::chrono::seconds(20));
     device->stop();
 
-    for (auto const &d : rSignal) {
-        rSignalFile << d << std::endl;
-    }
-
-// ------------------ extract data from signl -----------------
-    float threshold = 3;
-
-    std::cout << std::endl;
-    std::cout << "Extracting ..." << std::endl;
-
-    std::deque<float> buffer;
-    int state = 0; // 0 for preamble detection and 1 for data extraction
-    float lastBigSumI = 0;
-    for (auto i = 0; i < rSignal.size(); i++) {
-        if (state == 0) {
-            buffer.emplace_back(rSignal[i]);
-            if (buffer.size() < preamble.size()) {
-                continue;
-            }
-            float sum = 0;
-            for (auto j = 0; j < preamble.size(); j++) {
-                sum += buffer[j] * preamble[j];
-            }
-            buffer.pop_front();
-            if (sum > threshold && i - lastBigSumI > preamble.size()) {
-                lastBigSumI = i;
-                buffer.clear();
-                state = 1;
-            }
-        }
-        else if (state == 1) {
-            buffer.emplace_back(rSignal[i]);
-            if (buffer.size() < packetBits * carrierSize)
-                continue;
-            for (auto sampleIndex = 0; sampleIndex < packetBits * carrierSize; ) {
-                float sum = 0;
-                for (auto j = 0; j < carrierSize; sampleIndex++, j++) {
-                    sum += buffer[sampleIndex] * carrier[j];
-                }
-                rDataEncoded.emplace_back(sum > 0 ? 0 : 1);
-            }
-            buffer.clear();
-            state = 0;
-        }
-    }
-
-// --------------------- apply 8b10b decoding ---------------------
-    for (auto i = 0; i < rDataEncoded.size() / 10; i++) {
-       rData.push<8>(B8B10::decode(rDataEncoded.get<10>(i)));
-    }
-// ----------------------------------------------------------------
-
-    std::cout << std::endl;
-    std::cout << "Generating rData ..." << std::endl;
-
-    for (auto const &d : rData) {
-        rDataFile << d << std::endl;
-    }
-    std::cout << std::endl;
-
-    std::ofstream outputDataFile("OUTPUT.bin", std::ios::binary);
-    char byteOut;
-    for (auto const &byteOut : rData.as_span<char>()) {
-        outputDataFile.write(&byteOut, sizeof(byteOut));
-    }
 
     return 0;
 }
--- Applications/Project2/src/naive-sender.cpp
+++ Applications/Project2/src/naive-sender.cpp
@@ -26,10 +26,65 @@
 #include "asiodevice.h"
 using namespace ASIO;
 
-std::vector<float> preamble, carrier;
-std::vector<float> sSignal;
 
 class Sender : public IOHandler<float> {
+    
+    std::vector<float> preamble, carrier;
+    std::vector<float> sSignal;
+
+    const int packetBits = 500;
+    const int interSize = 10;
+    const int carrierSize = 2;
+    BitsContainer sDataEncoded;
+
+
+public:
+
+    Sender() {
+
+        std::ifstream inputDataFile("INPUT.bin", std::ios::binary);
+        BitsContainer inputData;
+        inputData.reserve(100000);
+        char byteIn;
+        while (inputDataFile.read(&byteIn, sizeof(byteIn))) {
+            inputData.push(byteIn);
+        }
+        std::ofstream sDataFileIn {"sData.txt"};
+        for (auto const &d : inputData) {
+            sDataFileIn << d << std::endl;
+        }
+
+        std::ofstream debugFile {"debug.txt"};
+        std::ifstream preambleFile {"preamble.txt"}, sDataFile {"sData.txt"};
+        BitsContainer sData;
+        preamble.reserve(100);
+        carrier.reserve(carrierSize);
+        sSignal.reserve(200000);
+        sDataEncoded.reserve(100000);
+
+        float preambleSample;
+        while (preambleFile >> preambleSample) {
+            preamble.emplace_back(preambleSample);
+        }
+
+        for (auto i = 0; i < carrierSize; i++) {
+            carrier.emplace_back(1);
+        }
+
+        bool dataBit;
+        while (sDataFile >> dataBit) {
+            sData.emplace_back(dataBit);
+        }
+
+    // --------------------- apply 8b10b encoding ---------------------
+        for (auto i = 0; i < sData.size() / 8; i++) {
+            sDataEncoded.push<10>(B8B10::encode(sData.get<8>(i)));
+        }
+
+        
+
+    }
+
     void outputCallback(DataView<float> &buffer) noexcept override {
         static auto sampleIndex = 0;
         for (auto i = 0; i < buffer.getNumSamples(); i++, sampleIndex++) {
@@ -37,73 +92,50 @@ class Sender : public IOHandler<float> {
             buffer(1, i) = sampleIndex < sSignal.size() ? sSignal[sampleIndex] : 0;
         }
     }
+
+
+    int i = 0;
     void inputCallback(const DataView<float> &buffer) noexcept override {
+
+        static bool first = true;
+        if (first) first = false;
+        else return;
+
+    // ------------------ generate the signal to send -----------------
+        float amp = 0.1;
+        for (; i < sDataEncoded.size(); i+=packetBits) {
+            for (auto j = 0; j < interSize; j++) {
+                sSignal.emplace_back(0);
+            }
+            for (auto j = 0; j < preamble.size(); j++) {
+                sSignal.emplace_back(preamble[j] * amp);
+            }
+            for (auto j = 0; j < packetBits; j++) {
+                for (auto k = 0; k < carrierSize; k++) {
+                    sSignal.emplace_back(carrier[k] * (sDataEncoded[i+j] == 0 ? amp : -amp));
+                }
+            }
+            for (auto j = 0; j < interSize; j++) {
+                sSignal.emplace_back(0);
+            }
+        }
+
+    }
+
+    ~Sender() {
+        std::ofstream sSignalFile {"sSignal.txt"};
+        for (auto const &d : sSignal) {
+            sSignalFile << d << std::endl;
+        }
+    }
+
+    auto duration() {
+        return std::chrono::seconds((int) sDataEncoded.size() * carrier.size() / 44100 + 2);
     }
 };
 
 
 int main() {
-
-    std::ifstream inputDataFile("INPUT.bin", std::ios::binary);
-    BitsContainer inputData;
-    inputData.reserve(100000);
-    char byteIn;
-    while (inputDataFile.read(&byteIn, sizeof(byteIn))) {
-        inputData.push(byteIn);
-    }
-    std::ofstream sDataFileIn {"sData.txt"};
-    for (auto const &d : inputData) {
-        sDataFileIn << d << std::endl;
-    }
-
-    std::ofstream debugFile {"debug.txt"};
-    std::ifstream preambleFile {"preamble.txt"}, sDataFile {"sData.txt"};
-    std::ofstream sSignalFile {"sSignal.txt"};
-    BitsContainer sData, sDataEncoded;
-    const int packetBits = 500, interSize = 10, carrierSize = 2;
-    preamble.reserve(100);
-    carrier.reserve(carrierSize);
-    sSignal.reserve(200000);
-    sDataEncoded.reserve(100000);
-
-    float preambleSample;
-    while (preambleFile >> preambleSample) {
-        preamble.emplace_back(preambleSample);
-    }
-
-    for (auto i = 0; i < carrierSize; i++) {
-        carrier.emplace_back(1);
-    }
-
-    bool dataBit;
-    while (sDataFile >> dataBit) {
-        sData.emplace_back(dataBit);
-    }
-
-// --------------------- apply 8b10b encoding ---------------------
-    for (auto i = 0; i < sData.size() / 8; i++) {
-       sDataEncoded.push<10>(B8B10::encode(sData.get<8>(i)));
-    }
-
-// ------------------ generate the signal to send -----------------
-    float amp = 0.1;
-    for (auto i = 0; i < sDataEncoded.size(); i+=packetBits) {
-        for (auto j = 0; j < interSize; j++) {
-            sSignal.emplace_back(0);
-        }
-        for (auto j = 0; j < preamble.size(); j++) {
-            sSignal.emplace_back(preamble[j] * amp);
-        }
-        for (auto j = 0; j < packetBits; j++) {
-            for (auto k = 0; k < carrierSize; k++) {
-                sSignal.emplace_back(carrier[k] * (sDataEncoded[i+j] == 0 ? amp : -amp));
-            }
-        }
-        for (auto j = 0; j < interSize; j++) {
-            sSignal.emplace_back(0);
-        }
-    }
-// ----------------------------------------------------------------
 
     auto sender = std::make_shared<Sender>();
     auto device = std::make_shared<Device>();
@@ -112,16 +144,14 @@ int main() {
     std::cout << std::endl;
     std::cout << "Generating sSignal ..." << std::endl;
 
-    for (auto const &d : sSignal) {
-        sSignalFile << d << std::endl;
-    }
 
     std::cout << std::endl;
     std::cout << "Starting device ..." << std::endl;
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
     device->start(sender);
-    std::this_thread::sleep_for(std::chrono::seconds((int) sDataEncoded.size() * carrier.size() / 44100 + 2));
+    std::this_thread::sleep_for(sender->duration());
+    // std::this_thread::sleep_for(std::chrono::seconds(10));
     device->stop();
 
     return 0;
